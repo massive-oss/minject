8062ea20ed67255d8c517a7dfbf4d04178c77cc9
Jozef Chutka <jozefchutka@gmail.com>
2018-04-23 09:49:37 +0200

    resolve conflicts

--- src/lib/minject/Injector.hx
+++ src/lib/minject/Injector.hx
@@ -38,7 +38,7 @@ class Injector
 	/**
 		The parent of this injector
 	**/
-	public var parent(default, null):Injector;
+	public var parent:Injector;
 
 	// map of injector mappings by type#name
 	var mappings = new Map<String, InjectorMapping<Dynamic>>();
@@ -387,9 +387,9 @@ class Injector
 				if (injected.indexOf(name) > -1) continue;
 				injected.push(name);
 
-				if (field.length == 3)
+				if (field.length == 4)
 				{
-					info.fields.push(new PropertyInjectionPoint(name, field[1], field[2]));
+					info.fields.push(new PropertyInjectionPoint(name, field[1], field[2], field[3] == '1'));
 				}
 				else if (name == 'new')
 				{
